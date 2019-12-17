af770b115ba92896cb7ecca645727415be3ddb76
Matheus <matheusguanaz@hotmail.com>
2019-12-17 18:09:24 -0200

    Tarefas do segundo dia

--- dia1/calculadora.js
+++ dia1/calculadora.js
@@ -18,7 +18,7 @@ router.post('/', (req,res)=>{
     let operador = req.body.operador;
 
     let resultado;
-    if(operador == '+') resultado = operando1 + operando2;
+    if(operador == '+') resultado = Number(operando1) + Number(operando2);
     else if(operador == '-') resultado = operando1 - operando2;
     else if(operador =='*') resultado = operando1 * operando2;
     else if(operador == '/') resultado = operando1 / operando2;
--- dia1/index.js
+++ dia1/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import calculadora from './calculadora';// utilizar ./ para pegar da mesma pasta
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/calculadora', calculadora);//nome utilizado no dns da aplicação ex: localhost:3000/calculadora
 
 app.listen(3000, () => // ouvir na porta 3000
