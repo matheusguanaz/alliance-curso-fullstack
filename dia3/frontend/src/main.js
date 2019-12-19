b93d4a32f91a0e6502e3b5f1e8496933760ab92c
Matheus <matheusguanaz@hotmail.com>
2019-12-19 17:31:38 -0300

    Trabalhos do dia 4

---
+++ dia3/frontend/src/main.js
@@ -0,0 +1,19 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/fontawesome'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+//import BootstrapVue from './bootstrap-vue'
+
+Vue.config.productionTip = false
+
+//Vue.use(BootstrapVue);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
